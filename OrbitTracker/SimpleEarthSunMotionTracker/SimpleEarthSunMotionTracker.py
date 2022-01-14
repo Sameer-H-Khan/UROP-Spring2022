9e681e6a1d29cf95fe6135bdc89dc94f729b3e39
Sameer-H-Khan <khan.sameerh2002@gmail.com>
2022-01-14 10:24:39 -0700

    Finished intial passthrough of logic

--- OrbitTracker/SimpleEarthSunMotionTracker/SimpleEarthSunMotionTracker.py
+++ OrbitTracker/SimpleEarthSunMotionTracker/SimpleEarthSunMotionTracker.py
@@ -11,6 +11,7 @@ G = 6.67408e-8
 def trackEarthMotion(numberOfYears, numberOfSteps):
 	tf = numberOfYears * 31557600.0		# Converting years to seconds. Using 365.25 days/year. May be slightly inaccurate
 	dt = tf / numberOfSteps
+	print(dt)
 
 	# Values for earth
 	x = np.zeros(numberOfSteps)
@@ -26,15 +27,11 @@ def trackEarthMotion(numberOfYears, numberOfSteps):
 	x[0] = X[0]; y[0] = Y[0]; z[0] = Z[0]; vx[0] = VX[0]; vy[0] = VY[0]; vz[0] = VZ[0];
 
 	# Acceleration should be negative if the earth's position is greater than the sun's position,
-	# positive if the earth's position is less than the sun's position
-	#ax[0] = G*M[1]/((X[1] - x[0])*np.abs(X[1] - x[0]))	
+	# positive if the earth's position is less than the sun's position	
 	r = np.sqrt(x[0]**2 + y[0]**2 + z[0]**2)
 	ax[0] =  -G*M[1]/r**2 * x[0]/r
-
-	# Because both the earth and the sun start out at y, z = 0, 0, finding the acceleration in these
-	# directions is weird because r = 0, so this will be ignored for now
-	#ay[0] = G*M[1]/((Y[1] - y[0])*np.abs(Y[1] - y[0]))		 
-	#az[0] = G*M[1]/((Z[1] - z[0])*np.abs(Z[1] - z[0]))
+	ay[0] =  -G*M[1]/r**2 * y[0]/r
+	az[0] =  -G*M[1]/r**2 * z[0]/r
 
 	for i in range(1, numberOfSteps):
 		x[i] = x[i-1] + vx[i-1]*dt
@@ -47,36 +44,35 @@ def trackEarthMotion(numberOfYears, numberOfSteps):
 
 		r = np.sqrt(x[i]**2 + y[i]**2 + z[i]**2)
 		acc = G*M[1]/r**2;
-		#if(X[1] - x[i] == 0):
-		#	ax[i] = 0
-		#else:
-		#ax[i] = G*M[1]/((X[1] - x[i])*np.abs(X[1] - x[i]))
+
 		ax[i] = acc * -x[i]/r	
-
-		#if(Y[1] - y[i] == 0):
-		#	ay[i] = 0
-		#else:
-		#ay[i] = G*M[1]/((Y[1] - y[i])*np.abs(Y[1] - y[i]))
 		ay[i] = acc * -y[i]/r
-
-		# I'm ignoring the z-component for now, just trying to get it to work in 2 dimensions first
-		#if(Z[1] - z[i] == 0):
-		#	az[i] = 0
-		#else:
-		#az[i] = G*M[1]/((Z[1] - z[i])*np.abs(Z[i] - z[i]))
+		az[i] = acc * -z[i]/r
 
 	return x, y, z, vx, vy, vz, ax, ay, az
 
-x_arr, y_arr, z_arr, vx_arr, vy_arr, vz_arr, ax_arr, ay_arr, az_arr = trackEarthMotion(0.5, 100)
-print("***********X Position**********")
-print(x_arr)	    
-print("***********Y Position**********")
-print(y_arr)	
-print("***********Y Velocity**********")
-print(vy_arr)	    
-print("***********Y Acceleration**********")
-print(ay_arr)
-plt.scatter(x_arr, y_arr)
+x_arr, y_arr, z_arr, vx_arr, vy_arr, vz_arr, ax_arr, ay_arr, az_arr = trackEarthMotion(1, 5000)
+#print("***********X Position**********")
+#print(x_arr)
+#print("***********X Velocity**********")
+#print(vx_arr)
+#print("***********X Acceleration**********")
+#print(ax_arr)	    
+
+#print("***********Y Position**********")
+#print(y_arr)	
+#print("***********Y Velocity**********")
+#print(vy_arr)	    
+#print("***********Y Acceleration**********")
+#print(ay_arr)
+
+plot1 = plt.figure(1)
+plt.scatter(x_arr, y_arr, s=1)
+
+plot2 = plt.figure(2)
+graph1 = plt.axes(projection = '3d')
+graph1.scatter3D(x_arr, y_arr, z_arr, s=1)
+
 plt.show()
 
 
